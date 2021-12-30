22563f1ff79fc46fa5387343ce0ec9a879217d9b
Scott Smith <ssmith@tmvse.com>
2021-12-30 16:15:37 +0000

    parallel day05

--- src/day05/day5.cpp
+++ src/day05/day5.cpp
@@ -52,25 +52,52 @@ auto part1(const std::string& in)
     return password;
 }
 
+
+struct hash2_t{
+    int pos;
+    int idx;
+    char c;
+};
+
 auto part2(const std::string& in)
 {
-    int i = 0;
-    int found = 0;
     std::string password(8, '_');
-    while(found < 8){
+    std::vector<std::vector<hash2_t>> per_thread(omp_get_max_threads(), std::vector<hash2_t>());
+
+    #pragma omp parallel for 
+    for(int i=0; i<28000000; ++i){
         std::string s = in + std::to_string(i);
         std::string h = md5(s);
         if(h[0] == '0' && h[1] == '0' && h[2] == '0' && h[3] == '0' && h[4] == '0'){
-            if(isdigit(h[5]) && (h[5]-'0') < 8 && password[h[5]-'0'] == '_'){
-                found++;
-                password[h[5]-'0'] = h[6];
-                //std::cout << h[5] << ", " << h[6] << ", " << password << std::endl;
+            if(isdigit(h[5]) && (h[5]-'0') < 8){
+                per_thread[omp_get_thread_num()].push_back({i, h[5]-'0', h[6]});
             }
         }
-        i++;
     }
 
-    return password;
+    std::vector<hash2_t> all_hashes;
+    for(auto& hashes : per_thread){
+        for(auto& hash : hashes){
+            all_hashes.push_back(hash);
+        }
+    }
+
+    std::sort(all_hashes.begin(), all_hashes.end(), [](auto& a,auto& b){
+        return a.pos < b.pos;
+    });
+
+    int found = 0;
+    for(auto& hash : all_hashes){
+        if(password[hash.idx] == '_'){
+            password[hash.idx] = hash.c;
+            found++;
+        }
+        if(found == 8){
+            return password;
+        }
+    }
+
+    return "fail: " + password;
 }
 
 void main()
